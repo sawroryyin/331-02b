c96834fd2f9d5c06f4fe55330bdebaa8fbdba248
saw_rory_yin <sawroryyinmky.26@gmail.com>
2025-07-03 18:04:40 +0700

    finish 9

--- 331-intro-to-vite/src/components/CategoryCard.vue
+++ 331-intro-to-vite/src/components/CategoryCard.vue
@@ -20,8 +20,9 @@ defineProps<{
 <template>
   <div class="event-class">
     <div class="event-card">
-      <h2>{{ event.title }}</h2>
-      <span>@{{ event.time }} on {{  event.date }}</span>
+      <span>{{ event.category }}</span>
+      <br>
+      <span>{{ event.organizer }}</span>
     </div>
   </div>
 </template>
@@ -33,6 +34,7 @@ defineProps<{
   cursor: pointer;
   border: 1px solid #39495c;
   margin-bottom: 18px;
+  text-align: right;
 }
 .event-card:hover {
   transform: scale(1.01);
