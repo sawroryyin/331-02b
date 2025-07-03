e4db89154eb419616bd4074805eebb091cfeabc7
saw_rory_yin <sawroryyinmky.26@gmail.com>
2025-07-03 17:28:10 +0700

    finish 7

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 // defineProps<{
 //   msg: string
 // }>()
+const event = ref({
+  id: 5928101,
+  category: 'animal welfare',
+  title: 'Cat Adoption Day',
+  description: 'Find your new feline friend at this event.',
+  location: 'Meow Town',
+  date: 'January 28, 2022',
+  time: '12:00',
+  petsAllowed: true,
+  organizer: 'Kat Laydee'
+})
 </script>
 
 <template>
-  <div class="event-class"></div>
+  <div class="event-class">
+    <div class="event-card">
+      <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{  event.date }}</span>
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -14,7 +31,7 @@
   width: 250px;
   cursor: pointer;
   border: 1px solid #39495c;
-  margin-bottom: 18 px;
+  margin-bottom: 18px;
 }
 .event-card:hover {
   transform: scale(1.01);
